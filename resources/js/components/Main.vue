c9a5582e22b6100a2810b2cf745276e68f664b4e
AntonyMorgante <antonymorgante1@gmail.com>
2022-03-24 17:07:22 +0100

    vue

---
+++ resources/js/components/Main.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="(post,index) in posts" :key=index>
+
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Main",
+    data (){
+        return {
+            posts: []
+        }
+    },
+    created(){
+
+    }
+}
+</script>
